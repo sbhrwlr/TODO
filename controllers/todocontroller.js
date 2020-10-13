e4fa7eccbf6561009ee869af4f25a5032884a4b6
Rahul Sabharwal <sabharwalrahul2000@gmail.com>
2020-10-13 20:52:25 +0530

    add and delete task

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,15 +1,25 @@
+var bodyparser = require('body-parser');
+
+var data = [{item: "Get Milk"},{item: "Walk Dog"},{item: "Fight Training"}];
+
+var urlencodedParser = bodyparser.urlencoded({extended: false});
+
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
-    res.render('todo');
+    res.render('todo', {todos: data});
 });
 
-app.post('/todo', function(req, res){
-    
+app.post('/todo', urlencodedParser, function(req, res){
+    data.push(req.body);
+    res.json(data);
 });
 
-app.delete('/todo', function(req, res){
-    
+app.delete('/todo/:item', function(req, res){
+    data=data.filter(function(todo){
+        return todo.item.replace(/ /g, '-')!== req.params.item;
+    });
+    res.json(data);
 });
 
 };
